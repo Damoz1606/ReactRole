7404b8b460fda2d448ef14c574e9ae8ef50eb563
Damoz1606 <panchitodmun@gmail.com>
2022-04-25 18:48:34 -0500

    Update appbar

--- front/src/components/Appbar.tsx
+++ front/src/components/Appbar.tsx
@@ -9,6 +9,38 @@ import { SessionManager } from '../managers/SessionManager';
 import { logout } from '../services/user.service';
 import { toastPromise } from '../utils/toast-manager';
 import { LOGOUT_MESSAGES } from '../utils/utils';
+import { CSSProperties } from '@mui/styled-engine';
+
+const appbar: any = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: ICON_SIZE * 2,
+    height: '100vh',
+    zIndex: 1000,
+}
+
+const innerContainer: any = {
+    background: PRIMARY.MAIN,
+    height: '100vh',
+    width: '50%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+}
+
+const outerContainer: any = {
+    background: PRIMARY.MAIN,
+    height: '100vh',
+    width: '90%',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
 
 interface Props {
     items: NavigationItem[];
@@ -29,30 +61,42 @@ function Appbar(props: Props) {
     }
 
     return <>
-        <div style={{ ...theme.container, ...theme.center, ...theme.column, backgroundColor: PRIMARY.MAIN, color: PRIMARY.CONTRAST }}>
-            <div style={{ position: 'absolute', top: 0 }}>
-                <ButtonBase style={{ ...theme.link, width: ICON_SIZE, height: ICON_SIZE, color: PRIMARY.CONTRAST }} disableRipple><Menu /></ButtonBase>
-            </div>
-            {props.items.map((item, index) => {
-                return <>
-                    <Link to={item.path} style={{ width: ICON_SIZE, height: ICON_SIZE, color: PRIMARY.CONTRAST }} key={index}>{item.icon}</Link>
-                </>
-            })}
-            <div style={{ position: 'absolute', bottom: 0 }}>
-                <ButtonBase style={{
-                    width: ICON_SIZE,
-                    height: ICON_SIZE,
-                    color: PRIMARY.CONTRAST,
-                    borderTopLeftRadius: '100px',
-                    borderBottomLeftRadius: '100px',
-                }}
-                    onClick={() => toastPromise({
-                        success: LOGOUT_MESSAGES.SUCCESS,
-                        pending: LOGOUT_MESSAGES.PENDING,
-                        error: LOGOUT_MESSAGES.ERROR
-                    },
-                        handleLogout())}
-                ><LogoutOutlined /></ButtonBase>
+        <div style={appbar}>
+            <div style={{ position: 'relative' }}>
+                <div style={innerContainer}></div>
+                <div style={outerContainer}>
+                    <div style={{ position: 'absolute', top: 0 }}>
+                        <ButtonBase style={{ ...theme.link, width: ICON_SIZE, height: ICON_SIZE, color: PRIMARY.CONTRAST }} disableRipple><Menu /></ButtonBase>
+                    </div>
+                    {props.items.map((item, index) => {
+                        return <>
+                            <Link 
+                            to={item.path} 
+                            style={{ 
+                                margin: '0.25rem auto',
+                                width: ICON_SIZE, 
+                                height: ICON_SIZE, 
+                                color: PRIMARY.CONTRAST }} 
+                            key={index}>{item.icon}</Link>
+                        </>
+                    })}
+                    <div style={{ position: 'absolute', bottom: 0 }}>
+                        <ButtonBase style={{
+                            width: ICON_SIZE,
+                            height: ICON_SIZE,
+                            color: PRIMARY.CONTRAST,
+                            borderTopLeftRadius: '100px',
+                            borderBottomLeftRadius: '100px',
+                        }}
+                            onClick={() => toastPromise({
+                                success: LOGOUT_MESSAGES.SUCCESS,
+                                pending: LOGOUT_MESSAGES.PENDING,
+                                error: LOGOUT_MESSAGES.ERROR
+                            },
+                                handleLogout())}
+                        ><LogoutOutlined /></ButtonBase>
+                    </div>
+                </div>
             </div>
         </div>
     </>

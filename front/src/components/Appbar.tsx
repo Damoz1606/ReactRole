d4712e8a3fe74554a691298189689f1d4d2bfcca
Damoz1606 <panchitodmun@gmail.com>
2022-04-26 09:51:13 -0500

    Change a little bit the appbar

--- front/src/components/Appbar.tsx
+++ front/src/components/Appbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PRIMARY } from '../style/color';
 import { ICON_SIZE, theme } from '../style/theme';
-import { Link, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { NavigationItem } from '../classes/NavigationItem';
 import { LogoutOutlined, Menu } from '@mui/icons-material';
 import { ButtonBase } from '@mui/material';
@@ -30,7 +30,6 @@ const innerContainer: any = {
 }
 
 const outerContainer: any = {
-    background: PRIMARY.MAIN,
     height: '100vh',
     width: '90%',
     position: 'absolute',
@@ -66,27 +65,53 @@ function Appbar(props: Props) {
                 <div style={innerContainer}></div>
                 <div style={outerContainer}>
                     <div style={{ position: 'absolute', top: 0 }}>
-                        <ButtonBase style={{ ...theme.link, width: ICON_SIZE, height: ICON_SIZE, color: PRIMARY.CONTRAST }} disableRipple><Menu /></ButtonBase>
+                        <ButtonBase style={{ 
+                            ...theme.link, 
+                            width: ICON_SIZE, 
+                            height: ICON_SIZE, 
+                            color: PRIMARY.CONTRAST,
+                            padding: '1rem',
+                            borderRadius: '100%',
+                            }}><Menu /></ButtonBase>
                     </div>
+                    <span style={{
+                        margin: '0 0.5rem',
+                        padding: '0.5rem',
+                        width: ICON_SIZE,
+                        background: PRIMARY.MAIN,
+                        height: '100%',
+                    }} />
                     {props.items.map((item, index) => {
                         return <>
-                            <Link 
-                            to={item.path} 
-                            style={{ 
-                                margin: '0.25rem auto',
-                                width: ICON_SIZE, 
-                                height: ICON_SIZE, 
-                                color: PRIMARY.CONTRAST }} 
-                            key={index}>{item.icon}</Link>
+                            <NavLink
+                                to={item.path}
+                                style={({ isActive }) => ({
+                                    margin: '0 0.5rem',
+                                    padding: '0.5rem',
+                                    width: ICON_SIZE,
+                                    height: ICON_SIZE,
+                                    backgroundColor: !isActive ? PRIMARY.MAIN : PRIMARY.CONTRAST,
+                                    color: !isActive ? PRIMARY.CONTRAST : PRIMARY.MAIN,
+                                    borderBottomLeftRadius: isActive ? '100px' : '0',
+                                    borderTopLeftRadius: isActive ? '100px' : '0',
+                                })}
+                                key={index}>{item.icon}</NavLink>
                         </>
                     })}
+                    <span style={{
+                        margin: '0 0.75rem',
+                        padding: '1.25rem',
+                        height: '100%',
+                        right: 0,
+                        background: PRIMARY.MAIN,
+                    }} />
                     <div style={{ position: 'absolute', bottom: 0 }}>
                         <ButtonBase style={{
-                            width: ICON_SIZE,
-                            height: ICON_SIZE,
+                            width: ICON_SIZE, 
+                            height: ICON_SIZE, 
                             color: PRIMARY.CONTRAST,
-                            borderTopLeftRadius: '100px',
-                            borderBottomLeftRadius: '100px',
+                            padding: '1rem',
+                            borderRadius: '100%',
                         }}
                             onClick={() => toastPromise({
                                 success: LOGOUT_MESSAGES.SUCCESS,
